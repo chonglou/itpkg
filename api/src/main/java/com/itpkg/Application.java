61f7dce2a01d5f4afe0b5745eb59e8db027ecbc4
flamen <jitang.zheng@gmail.com>
2015-07-14 21:23:57 -0700

    gradle project

--- api/src/main/java/com/itpkg/Application.java
+++ api/src/main/java/com/itpkg/Application.java
@@ -1,32 +1,8 @@
 package com.itpkg;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
-
 /**
  * Created by flamen on 15-7-14.
  */
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 public class Application {
-    @Bean
-    CommandLineRunner init() {
-        return (evt) -> Arrays.asList(
-                "root,admin".split(",")).forEach(
-                a -> {
-                    //todo 添加用户111
-                });
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
 }
