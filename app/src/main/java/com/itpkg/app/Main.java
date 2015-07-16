9b61d5f2494d88208ccc5e3511cfda19c0d9b04a
flamen <jitang.zheng@gmail.com>
2015-07-16 10:52:29 -0700

    package as jar

---
+++ app/src/main/java/com/itpkg/app/Main.java
@@ -0,0 +1,15 @@
+package com.itpkg.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by flamen on 15-7-16.
+ */
+public class Main {
+     private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Start application....");
+    }
+}
